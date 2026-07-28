03b134ea76e4b8c6cb9fb3b9afe499104385d0d3
Atharv Saxena <atharvsaxena786@gmail.com>
2026-07-28 22:29:02 +0530

    Time: 10 ms (31%), Space: 12.3 MB (62.8%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+
+        unordered_map<char,int>roman;
+
+        roman['I']=1;
+        roman['V']=5;
+        roman['X']=10;
+        roman['L']=50;
+        roman['C']=100;
+        roman['D']=500;
+        roman['M']=1000;
+
+        int answer=0;
+
+        for(int i =0;i< s.length();i++)
+        {
+            if(i< s.length()-1 && roman[s[i]] < roman[s[i+1]])
+            {
+
+            answer -= roman[s[i]];
+            }
+
+            else
+            {
+
+            answer += roman[s[i]];
+            }
+        }
+
+            return answer;
+    }
+};
